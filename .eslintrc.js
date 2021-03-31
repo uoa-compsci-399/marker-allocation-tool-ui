0344ef6e1c71dad03b54db37802741050e988a5c
Auclown <auclown@gmail.com>
2021-03-31 21:13:54 +1300

    Git Hook implemented

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,32 +5,54 @@ const defaultRules = [
 ];
 
 module.exports = {
-  extends: ["eslint-config-react-app"],
+  extends: ["airbnb", "plugin:prettier/recommended", "prettier"],
+  parser: "babel-eslint",
+  parserOptions: { sourceType: "module" },
   rules: {
-    'array-callback-return': 'warn',
-    'consistent-return': 'warn',
-    'default-case': 'warn',
-    // etc.
+    "no-use-before-define": "off",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    quotes: [2, "double", { avoidEscape: true }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
   },
-  overrides: [ //All extra typescript rules in this section
+  overrides: [
+    //All extra typescript rules in this section
     {
-      files: ['**/*.ts', '**/*.tsx'],
-      plugins: ['@typescript-eslint'],
+      files: ["**/*.ts", "**/*.tsx"],
+      plugins: ["@typescript-eslint"],
       extends: [
         ...defaultRules,
-        'plugin:@typescript-eslint/recommended',
+        "plugin:@typescript-eslint/recommended",
         // any other TypeScript specific config (from a plugin, or custom)
       ],
-      rules: { //These some examples of what we could have, this can be changed
-        '@typescript-eslint/no-explicit-any': 'warn',
-        '@typescript-eslint/no-unused-expressions': 'warn',
+      rules: {
+        //These some examples of what we could have, this can be changed
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-unused-expressions": "warn",
       },
     },
   ],
   settings: {
     react: {
       // React version. "detect" automatically picks the version you have installed.
-      version: 'detect',
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
     },
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import Example from './components/common-ui/example';
+import React from "react";
+import Example from "./components/common-ui/example";
 
 function App(): JSX.Element {
   return (
     <div className="flex max-w-full font-sans">
       <div className="grid max-w-full m-auto my-14">
         <div className="grid row-span-1 gap-4 my-5">
-            <Example title={"example 1"} description={"Provident similique accusantium nemo autem. Veritatis"}/>
-            <Example title={"example 2"} description={"Quo neque error repudiandae fuga? Ipsa laudantium molestias eos"}/>
+          <Example
+            title="example 1"
+            description="Provident similique accusantium nemo autem. Veritatis"
+          />
+          <Example
+            title="example 2"
+            description="Quo neque error repudiandae fuga? Ipsa laudantium molestias eos"
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
