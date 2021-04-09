308b02a882cc87a8db7ddb9f6249fabc3500e384
hsur753 <hsur753@aucklanduni.ac.nz>
2021-04-09 17:54:00 +1200

    configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,15 @@
 import React from 'react';
 
-import ApplicationView from './pages/ApplicationViewPage/';
-import ApplicationDetail from './pages/ApplicationDetailPage';
+import { Router } from 'react-router';
+import Routes from './routes';
 
-// Dummy ApplicationDetail data
-const dummyDataOne = {
-  title: 'Application #1',
-  name: 'Honger',
-  studentID: '18051980',
-  email: 'hong518@aucklanduni.ac.nz',
-  areaOfStudy: 'Computer Science',
-  currentYear: '3',
-  availabeSems: 'Semester One, Two',
-  experience: 'No',
-  visaStatus: 'Valid',
-  location: 'In Auckland',
-  prefCourse: 'COMPSCI 399',
-  files: 'Some files',
-};
-
-const dummyDataTwo = {
-  title: 'Application #2',
-  name: 'Applicant',
-  studentID: '584412',
-  email: 'appl487@aucklanduni.ac.nz',
-  areaOfStudy: 'Mathematics',
-  currentYear: '2',
-  availabeSems: 'Semester Two',
-  experience: 'Yes',
-  visaStatus: 'Valid',
-  location: 'Not in Auckland',
-  prefCourse: 'COMPSCI 367',
-  files: 'Some files',
-};
+import history from './services/history';
 
 function App(): JSX.Element {
   return (
-    <div>
-      <ApplicationView />
-      <ApplicationDetail info={dummyDataOne} />
-      <ApplicationDetail info={dummyDataTwo} />
-    </div>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import ApplicationList from '../pages/ApplicationViewPage/ApplicationList';
+import Example from '../components/common-ui/example';
+
+export default function Routes(): JSX.Element {
+  return (
+    <Switch>
+      <Route path="/" component={Example} />
+      <Route path="/applications" component={ApplicationList} />
+    </Switch>
+  );
+}
