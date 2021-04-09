fbd8bdab2db7da8c4a698b3b872236ea77677bfa
hsur753 <hsur753@aucklanduni.ac.nz>
2021-04-09 18:38:03 +1200

    update /applications route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import ApplicationList from '../pages/ApplicationViewPage/ApplicationList';
 import Example from '../components/common-ui/example';
+import ApplicationViewPage from '../pages/ApplicationViewPage';
 
 export default function Routes(): JSX.Element {
   return (
     <Switch>
       <Route path="/" exact component={Example} />
-      <Route path="/applications" component={ApplicationList} />
+      <Route path="/applications" component={ApplicationViewPage} />
     </Switch>
   );
 }
