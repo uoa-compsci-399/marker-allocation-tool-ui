97c389ac99b4f0f680e076d6906a34dc8c691341
hsur753 <hsur753@aucklanduni.ac.nz>
2021-04-17 23:06:07 +1200

    fix: hover state of BasicButton

--- src/components/common-ui/BasicButton.tsx
+++ src/components/common-ui/BasicButton.tsx
@@ -15,7 +15,7 @@ const BasicButton = ({
 }: BasicButtonProps): JSX.Element => {
   return (
     <button
-      className={`w-32 h-12 bg-${colour}-500 hover:bg-${colour}-600 text-${textColour} text-xl font-bold py-2 px-4 rounded`}
+      className={`w-32 h-12 bg-${colour}-500 hover:bg-${colour}-600 text-${textColour} text-xl font-bold py-2 px-4 rounded transition ease-in duration-200 focus:outline-none`}
     >
       {title}
     </button>
--- src/pages/ApplicationDetailPage/ButtonGroup.tsx
+++ src/pages/ApplicationDetailPage/ButtonGroup.tsx
@@ -4,7 +4,7 @@ import Button from '../../components/common-ui/BasicButton';
 
 const ButtonGroup = (): JSX.Element => {
   return (
-    <div className="text-center space-x-12 my-24 transition ease-in duration-200 focus:outline-none">
+    <div className="text-center space-x-12 my-24">
       <Button colour="green" title="Accept" />
       <Button colour="red" title="Reject" />
       <Button colour="yellow" title="Maybe" />
