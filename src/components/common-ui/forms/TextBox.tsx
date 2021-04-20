7f79ef70a342fc8e4329d8e36703c391d9eee48a
Lachlan McCrae <locky.mccrae@gmail.com>
2021-04-20 16:23:53 +1200

    css color quick fix

--- src/components/common-ui/forms/TextBox.tsx
+++ src/components/common-ui/forms/TextBox.tsx
@@ -9,7 +9,7 @@ const Textbox = ({ field, label }: TextboxProps): JSX.Element => {
   return (
     <div className="flex flex-col space-y-1">
       <label className="text-sm text-gray-900">{label}</label>
-      <Field className="p-1 border border-gray-300" type="textbox" id="firstName" name={field} />
+      <Field className="p-1 border border-gray-500" type="textbox" id="firstName" name={field} />
       <ErrorMessage name={field}>
         {(msg): JSX.Element => <div className="text-red-600">{msg}</div>}
       </ErrorMessage>
