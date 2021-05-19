0aeb8333255e454ccecdec63dbaf1782338e2877
Auclown <auclown@gmail.com>
2021-05-20 00:17:10 +1200

    FABpopup not being used for now, creating a new modal component

--- src/pages/ManageMarkersPage/FAB.tsx
+++ src/pages/ManageMarkersPage/FAB.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import useFetch from '../../hooks/useFetch';
-import FabPopup from './FABpopup';
+import React, { MouseEventHandler } from 'react';
+// import useFetch from '../../hooks/useFetch';
 
+interface FloatingButtonProps {
+  onClick: MouseEventHandler;
+}
 
-
-const FloatingButton = (): JSX.Element => {
-  const [showModal, setShowModal] = React.useState(false);
-
+const FloatingButton = ({ onClick }: FloatingButtonProps): JSX.Element => {
   return (
     <div className="fixed bottom-2 right-20">
       <button
         className="p-0 w-20 h-20 bg-blue-500 rounded-full hover:bg-blue-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
-        onClick={(): void => setShowModal(true)}
+        onClick={onClick}
       >
-        <svg viewBox="0 0 20 20" enable-background="new 0 0 20 20" className="w-6 h-6 inline-block">
+        <svg viewBox="0 0 20 20" enableBackground="new 0 0 20 20" className="w-6 h-6 inline-block">
           <path
             fill="#FFFFFF"
             d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
@@ -22,15 +21,8 @@ const FloatingButton = (): JSX.Element => {
           />
         </svg>
       </button>
-
-      {showModal ? (
-        <>
-          <FabPopup />
-        </>
-      ) : null}
     </div>
   );
 };
 
 export default FloatingButton;
-
--- src/pages/ManageMarkersPage/MarkersList.tsx
+++ src/pages/ManageMarkersPage/MarkersList.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import MarkerCard from './MarkerCard';
-import FloatingButton from './FAB';
 import { MarkerData } from '../../models/MarkerData';
+import MarkerCard from './MarkerCard';
 
 interface MarkersListProps {
   markerData: MarkerData[];
@@ -10,16 +9,13 @@ interface MarkersListProps {
 
 const MarkersList = ({ markerData }: MarkersListProps): JSX.Element => {
   const renderMarkers = markerData.map(({ markerId, name, mobile, email }) => (
-    <MarkerCard markerId={markerId} name={name} mobile={mobile} email={email} />
+    <MarkerCard markerId={markerId} name={name} mobile={mobile} email={email} key={markerId} />
   ));
 
   return (
     <div>
       <div className="text-center my-12 font-bold text-4xl">Current Markers</div>
       {renderMarkers}
-      <div className="text-right mx-24 my-12">
-        <FloatingButton />
-      </div>
     </div>
   );
 };
