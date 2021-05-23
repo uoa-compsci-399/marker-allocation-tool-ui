7f0da7eb3118598f50215fd9ed24cde338642497
hsur753 <hsur753@aucklanduni.ac.nz>
2021-05-23 18:16:36 +1200

    hook to useFetchSummaries.tsx

--- src/hooks/useFetchSummaries.tsx
+++ src/hooks/useFetchSummaries.tsx
@@ -1,23 +1,22 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FormApplication } from 'models/FormApplication';
+
+interface ResponseBody {
+  message: string;
+  data: FormApplication[];
+}
 
 const api_url = process.env.REACT_APP_API_DOMAIN;
 
-// fetches Application summaries
-
-function useFetchApplicant(id: string): [any, boolean] {
-  const [data, setData] = useState<string[]>([]);
+function useFetchApplicant(id: string): [ResponseBody, boolean] {
+  const [data, setData] = useState<ResponseBody>({ message: '', data: [] });
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    async function fetchApplication(): Promise<any> {
-      const json = await axios.get(`${api_url}/api/application/${id}`);
-      return json.data.data;
-    }
-
     async function fetchData(): Promise<void> {
-      const application = await fetchApplication();
-      setData(application);
+      const json = await axios.get(`${api_url}/api/course/${id}/applications`);
+      setData({ ...json.data });
       setLoading(false);
     }
 
