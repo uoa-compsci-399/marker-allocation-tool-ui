c805c7999a41c156926af291d553b8da20ecbfd2
Lachlan McCrae <locky.mccrae@gmail.com>
2021-05-26 17:41:36 +1200

    purge css fix

--- src/pages/ApplicationViewPage/Button.tsx
+++ src/pages/ApplicationViewPage/Button.tsx
@@ -2,6 +2,7 @@ interface ButtonProps {
   statusFilter: string;
   requiredFilter: string;
   colour: string;
+  colorHighlighted: string;
   setStatusFilter: any;
   label: string;
 }
@@ -10,10 +11,8 @@ const Button = (data: ButtonProps): JSX.Element => {
   return (
     <button
       className={`w-32 px-4 py-2 mx-3 text-lg font-bold text-white ${
-        data.statusFilter === data.requiredFilter
-          ? `bg-${data.colour}-600`
-          : `bg-${data.colour}-300`
-      } rounded-full hover:bg-${data.colour}-600`}
+        data.statusFilter === data.requiredFilter ? data.colorHighlighted : data.colour
+      } rounded-full hover:${data.colorHighlighted}`}
       onClick={(): void => data.setStatusFilter(data.requiredFilter)}
     >
       {data.label}
